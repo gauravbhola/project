1cf108be828c1741975cf5bbfce4b0fdea8d356b
gauav <bhola_g@hcl.com>
2021-05-21 11:06:20 +0530

    myfirst project

---
+++ src/main/java/HCLGroup/novartis/NovartisApplication.java
@@ -0,0 +1,13 @@
+package HCLGroup.novartis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovartisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovartisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HCLGroup/novartis/NovartisApplicationTests.java
@@ -0,0 +1,13 @@
+package HCLGroup.novartis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovartisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
